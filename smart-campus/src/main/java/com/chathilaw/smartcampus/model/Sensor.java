bea2cdda0e79539ed3793ef09dff84d30fbdf3b3
Chathila_Wijesinghe <chathilawijesinghe@gmail.com>
2026-04-19 23:51:40 +0530

    Updated Sensor.java
    - Created Sensor class to represent monitoring devices
    - Added attributes: id, type, status, currentValue, roomId
    - Implemented default and parameterized constructors
    - Added getters and setters for all fields
    - Linked sensor to Room using roomId (foreign key reference)

--- smart-campus/src/main/java/com/chathilaw/smartcampus/model/Sensor.java
+++ smart-campus/src/main/java/com/chathilaw/smartcampus/model/Sensor.java
@@ -6,8 +6,57 @@ package com.chathilaw.smartcampus.model;
 
 /**
  *
- * @author Chath
+ * @author Chathila Wijesinghe
  */
 public class Sensor {
+    private String id; // Unique identifier, e.g., "TEMP-001"
+    private String type; // Category, e.g., "Temperature", "Occupancy", "CO2"
+    private String status; // Current state: "ACTIVE", "MAINTENANCE", or "OFFLINE"
+    private double currentValue; // The most recent measurement recorded
+    private String roomId; // Foreign key linking to the Room where the sensor is located.
     
+    //constructors
+    public Sensor() {}
+    
+    public Sensor(String id, String type, String status, double currentValue, String roomId){
+        this.id = id;
+        this.type = type;
+        this.status = status;
+        this.currentValue = currentValue;
+        this.roomId = roomId;
+    }
+    
+    //getters
+    public String getId(){
+        return id;
+    }
+    public String getType(){
+        return type;
+    }
+    public String getStatus(){
+        return status;
+    }
+    public double getCurrentValue(){
+        return currentValue;
+    }
+    public String getRoomId(){
+        return roomId;
+    }
+    
+    //setters
+    public void setId(String id){
+        this.id = id;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public void setStatus(String status){
+        this.status = status;
+    }
+    public void setCurrentValue(double currentValue){
+        this.currentValue = currentValue;
+    }
+    public void setRoomId(String roomId){
+        this.roomId = roomId;
+    }
 }
