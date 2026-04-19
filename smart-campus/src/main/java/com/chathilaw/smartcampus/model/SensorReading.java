972a256bdd574ef773e6fea279c1516c9842cfb2
Chathila_Wijesinghe <chathilawijesinghe@gmail.com>
2026-04-19 23:55:14 +0530

    Updated SensorReading.java
    - Created SensorReading class to represent sensor data records
    - Added attributes: id, timestamp, value
    - Implemented default and parameterized constructors
    - Added getters and setters for all fields
    - Designed to store time-based sensor measurements

--- smart-campus/src/main/java/com/chathilaw/smartcampus/model/SensorReading.java
+++ smart-campus/src/main/java/com/chathilaw/smartcampus/model/SensorReading.java
@@ -6,8 +6,42 @@ package com.chathilaw.smartcampus.model;
 
 /**
  *
- * @author Chath
+ * @author Chathila Wijesinghe
  */
 public class SensorReading {
+    private String id; // Unique reading event ID (UUID recommended)
+    private long timestamp; // Epoch time (ms) when the reading was captured
+    private double value; // The actual metric value recorded by the hardware
     
+    //constructors
+    public SensorReading(){}
+    
+    public SensorReading(String id, long timestamp, double value){
+        this.id = id;
+        this.timestamp = timestamp;
+        this.value = value;
+    }
+    
+    //getters
+    public String getId(){
+        return id;
+    }
+    public long getTimestamp(){
+        return timestamp;
+    }
+    public double getValue(){
+        return value;
+    }
+    
+    //setters
+    public void setId(String id){
+        this.id = id;
+    }
+    public void setTimestamp(long timestamp){
+        this.timestamp = timestamp;
+    }
+    public void setValue(double value){
+        this.value = value;
+    }
+
 }
